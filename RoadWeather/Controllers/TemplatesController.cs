ad93044c08011215c1a6b9af8210277baf8df325
Michal Vanžura <vanzura.michal@gmail.com>
2014-11-16 17:55:20 +0100

    [ADD] marker Template

---
+++ RoadWeather/Controllers/TemplatesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoadWeather.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public ViewResult ViewMarkerTemplate()
+        {
+            return View();
+        }
+    }
+}
