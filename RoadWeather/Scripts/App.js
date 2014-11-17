461dfcde6028c0fc8c605750324fc8677d1135e8
Radim Fornusek <395918@mail.muni.cz>
2014-11-17 19:15:24 +0100

    Angular-resource to require

--- RoadWeather/Scripts/App.js
+++ RoadWeather/Scripts/App.js
@@ -1,4 +1,4 @@
-﻿var roadWeatherApp = angular.module('roadWeatherApp', ['google-maps'.ns(), 'ngAutocomplete', 'ngRoute','ui.bootstrap'])
+﻿var roadWeatherApp = angular.module('roadWeatherApp', ['google-maps'.ns(), 'ngAutocomplete', 'ngRoute','ngResource', 'ui.bootstrap'])
     .config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
         GoogleMapApi.configure({
             //    key: 'your api key',
