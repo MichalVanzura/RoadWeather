c454e36bf7b15c4b88ede99beca9ab84dd9dd337
KaterinaKonecna <katerinakonecna89@gmailcom>
2014-11-26 20:37:14 +0100

    Test

---
+++ RoadWeather.Test/Controllers/RoadWeatherTestControllers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadWeather.Test.Controllers
+{
+    [TestClass]
+    public class RoadWeatherTestControllers
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ RoadWeather.Test/Models/RoadWeatherTestModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadWeather.Test.Models
+{
+    [TestClass]
+    public class RoadWeatherTestModels
+    {
+        [TestMethod]
+        public void Trip_require_short_term_forecast()
+        {
+            //Arrange
+            RoadWeather.Models.Trip newTrip = new RoadWeather.Models.Trip()
+            {
+                Duration = 4,
+            };
+
+            //Act
+            bool isValid = RoadWeather.Managers.WeatherUtils.AvailableForShortTermForecast(newTrip);
+            
+            //Assert
+            Assert.IsTrue(isValid);
+
+        }
+    }
+}
