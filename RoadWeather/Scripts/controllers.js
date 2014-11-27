f013b0881ea81b5433b10f6e0d3a370763714efe
Radim Fornusek <395918@mail.muni.cz>
2014-11-27 23:18:56 +0100

    Set getdate if date not set

--- RoadWeather/Scripts/controllers.js
+++ RoadWeather/Scripts/controllers.js
@@ -74,6 +74,9 @@
                         directionsDisplay.setDirections(response);
                         directionsDisplay.setMap($scope.map.control.getGMap());
 
+                        if ($scope.dateTime == null) {
+                            $scope.dateTime = new Date();
+                        }
                         var message = getMessage.parseDirectionResult(response, $scope.dateTime);
                         getWeatherMarkers.getWeatherMarkers(message).success(
                             function (data) {
