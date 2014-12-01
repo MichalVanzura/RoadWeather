9c8ef2ef637f9851cb0c9c687c0bb888f7833e26
KaterinaKonecna <katerinakonecna89@gmailcom>
2014-12-01 15:03:44 +0100

    Test

--- RoadWeather.Test/Controllers/RoadWeatherTestControllers.cs
+++ RoadWeather.Test/Controllers/RoadWeatherTestControllers.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using RoadWeather.Controllers;
+using RoadWeather.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RoadWeather.Test.Controllers
@@ -6,9 +11,36 @@ namespace RoadWeather.Test.Controllers
     [TestClass]
     public class RoadWeatherTestControllers
     {
+
+        #region GetWeatherMarkers
+
         [TestMethod]
-        public void TestMethod1()
+        public void Test_GetWeatherMarkers_Return()
         {
+            //Arrange
+            Location location = new Location();
+            List<Location> list = new List<Location>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                list.Add(location);
+            }
+
+            Trip trip = new Trip();
+            trip.Locations = list;
+            trip.Duration = 72000;
+            trip.StartDateTime = new DateTime(2014, 12, 01, 20, 00, 00);
+
+            WeatherController temp = new WeatherController();
+
+            //Act
+            Task<List<Marker>> result = temp.GetWeatherMarkers(trip);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(Task<List<Marker>>));
+
         }
+
+        #endregion
     }
 }
