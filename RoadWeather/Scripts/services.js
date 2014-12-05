3a73ed23b063dcc75794d6902d40a9a59b465eca
Jmeno a Prijmeni <yanovskyy@ics.muni.cz>
2014-12-05 18:03:56 +0100

    update bubble message

--- RoadWeather/Scripts/services.js
+++ RoadWeather/Scripts/services.js
@@ -16,7 +16,6 @@
                 },
                 icon: image,
                 message: mark.Text,
-                message_bubble: "Description: " + mark.Text.Description + ", Temperature: " + mark.Text.Temperature + "°C, DateTime: " + mark.Text.DateTime,
                 
             };
 
